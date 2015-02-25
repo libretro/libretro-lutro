a49c40cc74bf28de60f81bbf2b9d9c9b903cc544
Higor Eurípedes <heuripedes@gmail.com>
2015-02-25 20:41:00 -0300

    Add some utility functions

--- runtime.h
+++ runtime.h
@@ -5,12 +5,6 @@
 #include "lua/src/lauxlib.h"
 #include "lua/src/lualib.h"
 
-int lutro_preload(lua_State *L, lua_CFunction f, const char *name);
-int lutro_ensure_global_table(lua_State *L, const char *name);
-void lutro_namespace(lua_State *L);
-
-void lutro_stack_dump(lua_State* L);
-
 /* functions to help check the stack size in a function call or block of code */
 #ifndef NDEBUG
 #define lutro_checked_stack_begin() int __stack = lua_gettop(L)
@@ -29,5 +23,32 @@ void lutro_stack_dump(lua_State* L);
 #define lutro_checked_stack_return(delta)
 #define lutro_checked_stack_end(delta)
 #endif
+
+int lutro_preload(lua_State *L, lua_CFunction f, const char *name);
+int lutro_ensure_global_table(lua_State *L, const char *name);
+void lutro_namespace(lua_State *L);
+void lutro_stack_dump(lua_State* L);
+
+/**
+ * @brief Import a lua module
+ *
+ * If an error occurs, the error string will be pushed to the stack.
+ *
+ * @param L
+ * @param modname    module name
+ * @param pop_result whether to pop the require() result
+ * @return whether the operation succeeded
+ */
+int lutro_require(lua_State* L, const char *modname, int pop_result);
+
+/**
+ * @brief Converts a relative path to a module name
+ *
+ * *outmod* must hold at least strlen(relpath)+1
+ *
+ * @param outmod  output module name
+ * @param relpath relative path to the module
+ */
+void lutro_relpath_to_modname(char *outmod, const char *relpath);
 
 #endif // RUNTIME_H

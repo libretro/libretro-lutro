f85df6526beb6af3fbd59ab66b746c2a4a92ef3b
Higor Eurípedes <heuripedes@gmail.com>
2015-02-26 09:10:16 -0300

    (live) Remove duplicate code in live_swap()

--- live.c
+++ live.c
@@ -35,14 +35,18 @@ void lutro_live_init()
 
    live.ifd = inotify_init1(IN_NONBLOCK);
 
-   if (live.ifd < 0) {
+   if (live.ifd < 0)
+   {
       fprintf(stderr, "disabling lutro.live due to inotify initialization failure: %s\n", strerror(errno));
       lutro_live_deinit();
       return;
    }
 
+   // XXX: Some editors do not trigger IN_MODIFY since they write to a temp file
+   //      and rename() it to the actual file.
    live.mfd = inotify_add_watch(live.ifd, settings.mainfile, IN_MODIFY);
-   if (live.mfd < 0) {
+   if (live.mfd < 0)
+   {
       fprintf(stderr, "lutro.live failed to monitor main.lua: %s\n", strerror(errno));
       lutro_live_deinit();
       return;
@@ -60,50 +64,57 @@ void lutro_live_deinit()
       close(live.ifd);
 }
 
+// copies stuff from table dst to table src
+static inline void shallow_update(lua_State *L, int src, int dst)
+{
+   lutro_checked_stack_begin();
+
+   assert(lua_istable(L, src));
+   assert(lua_istable(L, dst));
+
+   src = lua_absindex(L, src);
+   dst = lua_absindex(L, dst);
+
+   lua_pushnil(L);
+   while (lua_next(L, src))
+   {
+      lua_pushvalue(L, -2);
+      lua_insert(L, -2);
+      lua_settable(L, dst);
+   }
+
+   lutro_checked_stack_assert(0);
+}
+
 // TODO: check if there's anything else that needs to be backed up
 static void live_swap(lua_State *L)
 {
-   int idx = 0;
+   int backup = 0;
 
    lutro_checked_stack_begin();
 
-   // copy _G to "oldglobals"
+   // backup _G then drop it
    lua_pushglobaltable(L);
-   idx = lua_absindex(L, -1);
-
    lua_newtable(L);
-   lua_pushnil(L);
+   shallow_update(L, -2, -1);
+   lua_remove(L, -2);
 
-   while (lua_next(L, idx)) {
-      lua_pushvalue(L, -2);
-      lua_insert(L, -2);
-      lua_settable(L, -4);
-   }
-
-   // put _G on top and drop it
-   lua_insert(L, -2);
-   lua_pop(L, 1);
-
-   idx = lua_absindex(L, -1);
+   backup = lua_absindex(L, -1);
 
    if(luaL_dofile(L, settings.mainfile))
    {
       fprintf(stderr, "lutro.live lua error: %s\n", lua_tostring(L, -1));
-      lua_pop(L, 2); // error and "oldglobals"
-   } else {
-      // copy "oldglobals" to the new _G
+      lua_pop(L, 2); // error and backup
+   }
+   else
+   {
+      // restore _G then drop it and the backup
       lua_pushglobaltable(L);
-      lua_pushnil(L);
+      shallow_update(L, backup, -1);
+      lua_pop(L, 2);
 
-      while (lua_next(L, idx)) {
-         lua_pushvalue(L, -2);
-         lua_insert(L, -2);
-         lua_settable(L, -4);
-      }
-
-      lua_pop(L, 2); // _G and "oldglobals"
-
-      if (settings.live_call_load) {
+      if (settings.live_call_load)
+      {
          lua_getglobal(L, "lutro");
          lua_getfield(L, -1, "load");
 
@@ -132,7 +143,8 @@ void lutro_live_update(lua_State *L)
       modified = 1;
    }
 
-   if (errno != EINTR && errno != EAGAIN) {
+   if (errno != EAGAIN)
+   {
       fprintf(stderr, "innotify read error: %s\n", strerror(errno));
       return;
    }

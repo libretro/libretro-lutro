3297a4fbd1978643df3c2d867325c321b629c965
twinaphex <libretro@gmail.com>
2015-03-14 16:30:11 +0100

    Update libretro-common

--- libretro-common/formats/png/rpng_nbio.c
+++ libretro-common/formats/png/rpng_nbio.c
@@ -31,7 +31,7 @@
 #endif
 
 #include "rpng_common.h"
-#include "rpng_decode_common.h"
+#include "rpng_decode.h"
 
 static bool read_chunk_header(uint8_t *buf, struct png_chunk *chunk)
 {
@@ -56,9 +56,6 @@ static bool read_chunk_header(uint8_t *buf, struct png_chunk *chunk)
 static bool png_parse_ihdr(uint8_t *buf,
       struct png_ihdr *ihdr)
 {
-   unsigned i;
-   bool ret = true;
-
    buf += 4 + 4;
 
    ihdr->width       = dword_be(buf + 0);
@@ -70,70 +67,9 @@ static bool png_parse_ihdr(uint8_t *buf,
    ihdr->interlace   = buf[12];
 
    if (ihdr->width == 0 || ihdr->height == 0)
-      GOTO_END_ERROR();
+      return false;
 
-   if (ihdr->color_type == 2 || 
-         ihdr->color_type == 4 || ihdr->color_type == 6)
-   {
-      if (ihdr->depth != 8 && ihdr->depth != 16)
-         GOTO_END_ERROR();
-   }
-   else if (ihdr->color_type == 0)
-   {
-      static const unsigned valid_bpp[] = { 1, 2, 4, 8, 16 };
-      bool correct_bpp = false;
-
-      for (i = 0; i < ARRAY_SIZE(valid_bpp); i++)
-      {
-         if (valid_bpp[i] == ihdr->depth)
-         {
-            correct_bpp = true;
-            break;
-         }
-      }
-
-      if (!correct_bpp)
-         GOTO_END_ERROR();
-   }
-   else if (ihdr->color_type == 3)
-   {
-      static const unsigned valid_bpp[] = { 1, 2, 4, 8 };
-      bool correct_bpp = false;
-
-      for (i = 0; i < ARRAY_SIZE(valid_bpp); i++)
-      {
-         if (valid_bpp[i] == ihdr->depth)
-         {
-            correct_bpp = true;
-            break;
-         }
-      }
-
-      if (!correct_bpp)
-         GOTO_END_ERROR();
-   }
-   else
-      GOTO_END_ERROR();
-
-#ifdef RPNG_TEST
-   fprintf(stderr, "IHDR: (%u x %u), bpc = %u, palette = %s, color = %s, alpha = %s, adam7 = %s.\n",
-         ihdr->width, ihdr->height,
-         ihdr->depth, ihdr->color_type == 3 ? "yes" : "no",
-         ihdr->color_type & 2 ? "yes" : "no",
-         ihdr->color_type & 4 ? "yes" : "no",
-         ihdr->interlace == 1 ? "yes" : "no");
-#endif
-
-   if (ihdr->compression != 0)
-      GOTO_END_ERROR();
-
-#if 0
-   if (ihdr->interlace != 0) /* No Adam7 supported. */
-      GOTO_END_ERROR();
-#endif
-
-end:
-   return ret;
+   return true;
 }
 
 static bool png_realloc_idat(const struct png_chunk *chunk, struct idat_buffer *buf)
@@ -204,6 +140,9 @@ bool rpng_nbio_load_image_argb_iterate(uint8_t *buf, struct rpng_t *rpng)
          if (!png_parse_ihdr(buf, &rpng->ihdr))
             return false;
 
+         if (!png_process_ihdr(&rpng->ihdr))
+            return false;
+
          rpng->has_ihdr = true;
          break;
 
@@ -225,7 +164,7 @@ bool rpng_nbio_load_image_argb_iterate(uint8_t *buf, struct rpng_t *rpng)
          break;
 
       case PNG_CHUNK_IDAT:
-         if (!(rpng->has_ihdr) || rpng->has_iend || (rpng->ihdr.color_type == 3 && !(rpng->has_plte)))
+         if (!(rpng->has_ihdr) || rpng->has_iend || (rpng->ihdr.color_type == PNG_IHDR_COLOR_PLT && !(rpng->has_plte)))
             return false;
 
          if (!png_realloc_idat(&rpng->chunk, &rpng->idat_buf))
@@ -252,56 +191,17 @@ bool rpng_nbio_load_image_argb_iterate(uint8_t *buf, struct rpng_t *rpng)
    return true;
 }
 
-bool rpng_nbio_load_image_argb_process(struct rpng_t *rpng,
+int rpng_nbio_load_image_argb_process(struct rpng_t *rpng,
       uint32_t **data, unsigned *width, unsigned *height)
 {
-   if (!rpng)
-      return false;
-
-   rpng->process.inflate_buf_size = 0;
-   rpng->process.inflate_buf      = NULL;
-
-   if (inflateInit(&rpng->process.stream) != Z_OK)
-      return false;
-
-   png_pass_geom(&rpng->ihdr, rpng->ihdr.width,
-         rpng->ihdr.height, NULL, NULL, &rpng->process.inflate_buf_size);
-   if (rpng->ihdr.interlace == 1) /* To be sure. */
-      rpng->process.inflate_buf_size *= 2;
-
-   rpng->process.inflate_buf = (uint8_t*)malloc(rpng->process.inflate_buf_size);
-   if (!rpng->process.inflate_buf)
-      return false;
-
-   rpng->process.stream.next_in   = rpng->idat_buf.data;
-   rpng->process.stream.avail_in  = rpng->idat_buf.size;
-   rpng->process.stream.avail_out = rpng->process.inflate_buf_size;
-   rpng->process.stream.next_out  = rpng->process.inflate_buf;
-
-   if (inflate(&rpng->process.stream, Z_FINISH) != Z_STREAM_END)
+   if (!rpng->process.initialized)
    {
-      inflateEnd(&rpng->process.stream);
-      return false;
+      if (!rpng_load_image_argb_process_init(rpng, data, width,
+               height))
+         return PNG_PROCESS_ERROR;
    }
-   inflateEnd(&rpng->process.stream);
 
-   *width  = rpng->ihdr.width;
-   *height = rpng->ihdr.height;
-#ifdef GEKKO
-   /* we often use these in textures, make sure they're 32-byte aligned */
-   *data = (uint32_t*)memalign(32, rpng->ihdr.width * 
-         rpng->ihdr.height * sizeof(uint32_t));
-#else
-   *data = (uint32_t*)malloc(rpng->ihdr.width * 
-         rpng->ihdr.height * sizeof(uint32_t));
-#endif
-   if (!*data)
-      return false;
-
-   if (!png_reverse_filter_loop(rpng, data))
-      return false;
-
-   return true;
+   return png_reverse_filter_iterate(rpng, data);
 }
 
 void rpng_nbio_load_image_free(struct rpng_t *rpng)

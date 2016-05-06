37a7b97e3f95fa9707537ab36250fd122e68499e
Rob Loach <robloach@gmail.com>
2016-05-06 11:00:35 -0400

    Add lutro.event.quit()

---
+++ event.c
@@ -0,0 +1,44 @@
+#include "event.h"
+#include "lutro.h"
+#include "libretro.h"
+
+#include <stdlib.h>
+#include <string.h>
+
+int lutro_event_preload(lua_State *L)
+{
+   static luaL_Reg event_funcs[] =  {
+      { "quit", event_quit },
+      {NULL, NULL}
+   };
+
+   lutro_ensure_global_table(L, "lutro");
+
+   luaL_newlib(L, event_funcs);
+
+   lua_setfield(L, -2, "event");
+
+   return 1;
+}
+
+void lutro_event_init()
+{
+}
+
+/**
+ * lutro.event.quit()
+ *
+ * https://love2d.org/wiki/love.event.quit
+ */
+int event_quit(lua_State *L)
+{
+    int n = lua_gettop(L);
+    if (n > 1) {
+        return luaL_error(L, "lutro.event.quit requires 0 or 1 arguments, %d given.", n);
+    }
+
+    // Quit, ignoring the exit status.
+    retro_shutdown_game();
+
+    return 0;
+}

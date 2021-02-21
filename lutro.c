48f06d8513416040762ef518dc7f4bb3e3819577
Jean-André Santoni <jean.andre.santoni@gmail.com>
2021-02-21 12:58:32 +0700

    Fix indentation

--- lutro.c
+++ lutro.c
@@ -116,55 +116,55 @@ static void dumpstack( lua_State* L )
 #define LEVELS2	10	/* size of the second part of the stack */
 
 static int db_errorfb (lua_State *L) {
-  int level;
-  int firstpart = 1;  /* still before eventual `...' */
-  int arg = 0;
-  lua_State *L1 = L; /*getthread(L, &arg);*/
-  lua_Debug ar;
-  if (lua_isnumber(L, arg+2)) {
-    level = (int)lua_tointeger(L, arg+2);
-    lua_pop(L, 1);
-  }
-  else
-    level = (L == L1) ? 1 : 0;  /* level 0 may be this own function */
-  if (lua_gettop(L) == arg)
-    lua_pushliteral(L, "");
-  else if (!lua_isstring(L, arg+1)) return 1;  /* message is not a string */
-  else lua_pushliteral(L, "\n");
-  lua_pushliteral(L, "stack traceback:");
-  while (lua_getstack(L1, level++, &ar)) {
-    if (level > LEVELS1 && firstpart) {
-      /* no more than `LEVELS2' more levels? */
-      if (!lua_getstack(L1, level+LEVELS2, &ar))
-        level--;  /* keep going */
-      else {
-        lua_pushliteral(L, "\n\t...");  /* too many levels */
-        while (lua_getstack(L1, level+LEVELS2, &ar))  /* find last levels */
-          level++;
+   int level;
+   int firstpart = 1;  /* still before eventual `...' */
+   int arg = 0;
+   lua_State *L1 = L; /*getthread(L, &arg);*/
+   lua_Debug ar;
+   if (lua_isnumber(L, arg+2)) {
+      level = (int)lua_tointeger(L, arg+2);
+      lua_pop(L, 1);
+   }
+   else
+      level = (L == L1) ? 1 : 0;  /* level 0 may be this own function */
+   if (lua_gettop(L) == arg)
+      lua_pushliteral(L, "");
+   else if (!lua_isstring(L, arg+1)) return 1;  /* message is not a string */
+   else lua_pushliteral(L, "\n");
+   lua_pushliteral(L, "stack traceback:");
+   while (lua_getstack(L1, level++, &ar)) {
+      if (level > LEVELS1 && firstpart) {
+         /* no more than `LEVELS2' more levels? */
+         if (!lua_getstack(L1, level+LEVELS2, &ar))
+            level--;  /* keep going */
+         else {
+            lua_pushliteral(L, "\n\t...");  /* too many levels */
+            while (lua_getstack(L1, level+LEVELS2, &ar))  /* find last levels */
+               level++;
+         }
+         firstpart = 0;
+         continue;
       }
-      firstpart = 0;
-      continue;
-    }
-    lua_pushliteral(L, "\n\t");
-    lua_getinfo(L1, "Snl", &ar);
-    lua_pushfstring(L, "%s:", ar.short_src);
-    if (ar.currentline > 0)
-      lua_pushfstring(L, "%d:", ar.currentline);
-    if (*ar.namewhat != '\0')  /* is there a name? */
-        lua_pushfstring(L, " in function " LUA_QS, ar.name);
-    else {
+      lua_pushliteral(L, "\n\t");
+      lua_getinfo(L1, "Snl", &ar);
+      lua_pushfstring(L, "%s:", ar.short_src);
+      if (ar.currentline > 0)
+         lua_pushfstring(L, "%d:", ar.currentline);
+      if (*ar.namewhat != '\0')  /* is there a name? */
+         lua_pushfstring(L, " in function " LUA_QS, ar.name);
+      else {
       if (*ar.what == 'm')  /* main? */
-        lua_pushfstring(L, " in main chunk");
+         lua_pushfstring(L, " in main chunk");
       else if (*ar.what == 'C' || *ar.what == 't')
-        lua_pushliteral(L, " ?");  /* C function or tail call */
+         lua_pushliteral(L, " ?");  /* C function or tail call */
       else
-        lua_pushfstring(L, " in function <%s:%d>",
+         lua_pushfstring(L, " in function <%s:%d>",
                            ar.short_src, ar.linedefined);
-    }
-    lua_concat(L, lua_gettop(L) - arg);
-  }
-  lua_concat(L, lua_gettop(L) - arg);
-  return 1;
+      }
+      lua_concat(L, lua_gettop(L) - arg);
+   }
+   lua_concat(L, lua_gettop(L) - arg);
+   return 1;
 }
 
 static int dofile(lua_State *L, const char *path)
